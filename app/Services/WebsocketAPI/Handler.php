a681f060ecd3a866c376a4d917a8bf921a3748ad
yasser belhimer <yasserbelhimer@gmail.com>
2024-12-26 19:50:53 +0100

    reworked the reception

--- app/Services/WebsocketAPI/Handler.php
+++ app/Services/WebsocketAPI/Handler.php
@@ -24,14 +24,21 @@ class Handler
         if ($number) {
             $sending_server = $number->sendingServer;
 
-            $chatbox = ChatBox::firstOrNew([
+            $chatbox = ChatBox::where([
                 'user_id'           => $number->user_id,
-                'from'              => $number->number,
-                'to'                => $sender,
                 'sending_server_id' => $sending_server->id,
-            ]);
+            ])->where(function($query) use ($sender) {
+                $query->where('from', $sender)->orWhere('to', $sender);
+            })->first();
 
+            
             if (! $chatbox->exists) {
+                $chatbox = new ChatBox([
+                    'user_id'           => $number->user_id,
+                    'from'              => $number->number,
+                    'to'                => $sender,
+                    'sending_server_id' => $sending_server->id,
+                ]);
                 $chatbox->reply_by_customer = true;
                 $chatbox->save();
             }
@@ -39,7 +46,7 @@ class Handler
             $messageData  = [
                 'box_id'            => $chatbox->id,
                 'message'           => $content,
-                'send_by'           => 'to',
+                'send_by'           => $chatbox->from == $sender ? 'from' : 'to',
                 'sms_type'          => 'plain',
                 'sending_server_id' => $sending_server->id,
                 'external_uuid'     => $messageId,
